476095a305e8472f18e5905a1aa364f646832d80
Bikash <info.bikash9@gmail.com>
2024-01-30 15:30:09 +0530

    Create the object of chrome driver

--- src/test/java/com/taspro/base/TestBase.java
+++ src/test/java/com/taspro/base/TestBase.java
@@ -1,11 +1,13 @@
 package com.taspro.base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestBase {
 	
 	public void initDriver() {
 		WebDriver driver;
+		driver = new ChromeDriver();
 	}
 
 }
