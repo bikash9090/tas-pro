726b4cdfd54395994b5c71480bbb5bd50fff3933
Dipak Bhagat <dipakbhagatjobs@gmail.com>
2024-01-31 12:27:21 +0530

    Added some common actions

--- src/test/java/com/taspro/base/PageBase.java
+++ src/test/java/com/taspro/base/PageBase.java
@@ -1,5 +1,39 @@
 package com.taspro.base;
 
-public class PageBase {
+import java.time.Duration;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PageBase {
+	WebDriverWait wait;
+	public WebDriver driver;
+	
+
+	/*-------------------------------------------Page initialization----------------------------------------------*/
+	public PageBase(WebDriver driver) {
+		this.driver=driver;
+		
+	}
+/*----------------------------------------------------------------------------------------------------------------------------------*/
+	protected void scrollAndEnterText(WebElement element, String text) {
+		element.sendKeys(text);
+	}
+
+	protected void scrollAndClick(WebElement element) {
+		element.click();
+	}
+	protected void justWait() {
+		 wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		}
+	
+	protected void waitForElementToBeVisible(WebElement element) {
+		   wait.until(ExpectedConditions.visibilityOf(element));
+      
 }
+	
+	
+}
+	
