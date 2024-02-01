51029d4c5ea4bddde23df3a696de2e63bfddadab
Bikash <info.bikash9@gmail.com>
2024-02-01 12:21:48 +0530

    Modefied according requrements.

--- src/test/java/com/taspro/utility/ExcelUtil.java
+++ src/test/java/com/taspro/utility/ExcelUtil.java
@@ -8,38 +8,36 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelUtil {
-	
+
 	XSSFWorkbook workbook;
+
+	public String[][] readExcel(String sheetName) {
+		
+		String path = System.getProperty("user.dir")+"src\\test\\resources\\TAS-2_Employee_data.xlsx";
 	
-	
-	public String[][] readExcel() {
-	String path = "C:\\Users\\Subha Sangita Swain\\git\\tas-pro\\src\\test\\resources\\TAS-2_Employee_data.xlsx";
-	
-	try {
-		FileInputStream fis = new FileInputStream(path);
-		 workbook = new XSSFWorkbook(fis);
-	} catch (FileNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	
-	XSSFSheet sheet = workbook.getSheet("empSheet");
-	
-	int lastRow = sheet.getLastRowNum();
-	int lastCell = sheet.getRow(lastRow).getLastCellNum();
-	
-	String[][] empDetail = new String[lastRow][lastCell];
-	
-	for(int i=0;i<lastRow;i++) {
-		for(int j=0;j<lastCell;j++) {
-			empDetail[i][j] = sheet.getRow(i+1).getCell(j).toString();
+		try {
+			FileInputStream fis = new FileInputStream(path);
+			workbook = new XSSFWorkbook(fis);
+		} catch (FileNotFoundException e) {
+			System.out.println("Excel file not found! on the location : "+path);
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Input output error occured during excel file operations.");
+			e.printStackTrace();
 		}
+		
+		XSSFSheet sheet = workbook.getSheet(sheetName);
+		
+		int lastRow = sheet.getLastRowNum();
+		int lastCell = sheet.getRow(lastRow).getLastCellNum();
+
+		String[][] empDetail = new String[lastRow][lastCell]; // Create 2 dimensional array to store excel sheet data.
+
+		for (int i = 0; i < lastRow; i++) {
+			for (int j = 0; j < lastCell; j++) {
+				empDetail[i][j] = sheet.getRow(i + 1).getCell(j).toString(); //Retrieving and storing the data from excel sheet by skipping the first row.
+			}
+		}
+		return empDetail;
 	}
-	
-	return empDetail;
-	
-}
 }
