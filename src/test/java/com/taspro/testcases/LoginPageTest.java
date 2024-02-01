e63a9a4e7f9e2cd8ee0af54222bf4dc993f7e9ac
Dipak Bhagat <dipakbhagatjobs@gmail.com>
2024-02-01 12:28:50 +0530

    Driver initialization of before and after method

--- src/test/java/com/taspro/testcases/LoginPageTest.java
+++ src/test/java/com/taspro/testcases/LoginPageTest.java
@@ -1,5 +1,40 @@
 package com.taspro.testcases;
 
-public class LoginPageTest {
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.taspro.base.TestBase;
+import com.taspro.pages.LoginPage;
+import com.taspro.utility.PropertiesUtil;
+
+public class LoginPageTest extends TestBase {
+
+	LoginPage lpagloginpageObj;
+
+
+	@BeforeMethod
+	public void openBrowser() {
+		initiDriver();
+		lpagloginpageObj = new LoginPage(driver);
+		
+		
+	}
+	
+	@AfterMethod
+	public void closeBrowser() {
+		driver.quit();
+	}
+
+	@Test
+	public void loginFunTest() {
+		lpagloginpageObj.loginToUserAccount(readpropobj.getemail(),readpropobj.getpassword());
+	}
+
+	@Test
+	public void validateforgotpasswordlink() {
+		lpagloginpageObj.clickOnForgotPassword();
+	}
+	
 
 }
