f634bbcb22de7ed1ecdaa9dd37da1d77980390a5
dbhagat800 <dipakbhagatjobs@gmail.com>
2024-02-02 14:21:55 +0530

    added the modified structure

--- src/test/java/com/taspro/base/TestBase.java
+++ src/test/java/com/taspro/base/TestBase.java
@@ -2,25 +2,58 @@ package com.taspro.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 import com.taspro.utility.PropertiesUtil;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestBase {
-	
-	public WebDriver driver;
-	public PropertiesUtil readpropobj;
 
-	
-	public void initiDriver() {
-		readpropobj = new PropertiesUtil();
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		driver.get(readpropobj.getUrl());
+	public WebDriver driver;
+	public PropertiesUtil readpropobj = new PropertiesUtil();
+
+	String browser = readpropobj.getBrowser();
+	String url = readpropobj.getUrl();
+
+	/*------------------------------------------------Browser Setup----------------------------------------------*/
+	public WebDriver setUp(){
+		if(browser.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();	
+			
+		}
+			else if (browser.equalsIgnoreCase("firefox")){
+				WebDriverManager.firefoxdriver().setup();
+				driver = new FirefoxDriver();
+				
+			}
+				else if(browser.equalsIgnoreCase("edge")) {
+					WebDriverManager.edgedriver().setup();
+					driver = new EdgeDriver();	
+				
+				}
 		driver.manage().window().maximize();
-		readpropobj = new PropertiesUtil();
+		return driver;
+				
+			}
+	/*------------------------------------------------LAUNCHING THE WEBSITE----------------------------------------------*/
+	public void  launchWebSite() {
+        driver =setUp();
+		driver.get(url);
 		
 	}
+	/*------------------------------------------------CLOSING THE BROWSER----------------------------------------------*/
+	public void tearDown() {
+		driver.close();
+
+	}
+		
 
 }
+
