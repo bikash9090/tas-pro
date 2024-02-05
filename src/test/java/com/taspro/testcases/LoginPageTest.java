e8769ccce840141b802f8e33b53f69e8aaa414a4
Bikash <info.bikash9@gmail.com>
2024-02-05 11:56:08 +0530

    Removed unnecessary code.

--- src/test/java/com/taspro/testcases/LoginPageTest.java
+++ src/test/java/com/taspro/testcases/LoginPageTest.java
@@ -9,26 +9,24 @@ import com.taspro.pages.LoginPage;
 
 public class LoginPageTest extends TestBase {
 	LoginPage lpagloginpageObj;
-	
-	
-	
-	
-	
-	/*------------------------------------------------BeforClass initialization----------------------------------------------*/
+
+	/*-----------------------------------------BeforClass initialization-------------------------------------*/
 	@BeforeClass
 	public void initilization() {
 		launchWebSite();
 		lpagloginpageObj = new LoginPage(driver);
+		
 	}
-	/*------------------------------------------------Closing the browser after the test ----------------------------------------------*/
+
+	/*----------------------------------Closing the browser after all the tests-------------------------------*/
 	@AfterClass
 	public void close() {
 		tearDown();
 	}
-	/*------------------------------------------------TESTCASES----------------------------------------------*/
+
+	/*-------------------------------------------------TESTCASES----------------------------------------------*/
 	@Test
 	public void loginFunTest() {
-
 		lpagloginpageObj.loginToUserAccount(readpropobj.getemail(), readpropobj.getpassword());
 	}
 
@@ -36,21 +34,4 @@ public class LoginPageTest extends TestBase {
 	public void validateforgotpasswordlink() {
 		lpagloginpageObj.clickOnForgotPassword();
 	}
-	
-	
-	
-	
-
-//	@Test
-//	public void readexcel () {
-//		ExcelUtil obj = new ExcelUtil();
-//		String a[][]= obj.readExcel("empSheet");
-//		for(int i=0;i<a.length;i++) {
-//			for(int j=0;j<a[i].length;j++) {
-//				System.out.print(a[i][j]+" ");
-//			}
-//			System.out.println();
-//		}
-//		
-//	}
 }
