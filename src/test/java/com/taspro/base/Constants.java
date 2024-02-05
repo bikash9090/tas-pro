074413755f8202a9e81065bd035ee7e6570c41a4
Subha <sangitasubha466@gmail.com>
2024-02-05 18:20:01 +0530

    Created Constants Class

---
+++ src/test/java/com/taspro/base/Constants.java
@@ -0,0 +1,12 @@
+package com.taspro.base;
+
+public class Constants {
+	
+	
+	
+	public static final String USER_DIR = System.getProperty("user.dir");
+	public static final String SCREENSHOT_DIR = USER_DIR+"\\Screenshots\\";
+	public static final String EXCEL_PATH = USER_DIR+"src\\test\\resources\\TAS-2_Employee_data.xlsx";
+	public static final String PROPERTIES_FILE_PATH = USER_DIR+"\\src\\test\\resources\\config.properties";
+
+}
