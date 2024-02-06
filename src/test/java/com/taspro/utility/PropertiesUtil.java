a0e8d67f345528c99e24311fc76de8105d428ba4
Bikash <info.bikash9@gmail.com>
2024-02-06 10:18:03 +0530

    modified the path to dynamic path allocation.

--- src/test/java/com/taspro/utility/PropertiesUtil.java
+++ src/test/java/com/taspro/utility/PropertiesUtil.java
@@ -9,12 +9,12 @@ public class PropertiesUtil {
 	Properties pro;
 
 	public PropertiesUtil() {
-	
+
+		String path = System.getProperty("user.dir") + "\\src\\test\\resources\\config.properties";
 		pro = new Properties();
 		FileInputStream filepa;
 		try {
-			filepa = new FileInputStream(
-					"C:\\Users\\dipak\\git\\tas-pro\\src\\test\\resources\\config.properties");
+			filepa = new FileInputStream(path);
 			pro.load(filepa);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -23,8 +23,7 @@ public class PropertiesUtil {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 	}
 
 	public String getemail() {
@@ -34,9 +33,11 @@ public class PropertiesUtil {
 	public String getpassword() {
 		return pro.getProperty("password");
 	}
+
 	public String getUrl() {
-		 return pro.getProperty("url");
+		return pro.getProperty("url");
 	}
+
 	public String getBrowser() {
 		return pro.getProperty("browser");
 	}
