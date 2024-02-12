9846043e59314c43b5fd5e73f6b379732a69a40d
db2116 <db2116@checkboxtechnology.com>
2024-02-12 13:10:21 +0530

    changed the after and before class tomethod

--- src/test/java/com/taspro/testcases/LoginPageTest.java
+++ src/test/java/com/taspro/testcases/LoginPageTest.java
@@ -1,24 +1,29 @@
 package com.taspro.testcases;
 
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.taspro.base.TestBase;
+import com.taspro.pages.DashboardPage;
 import com.taspro.pages.LoginPage;
 
 public class LoginPageTest extends TestBase {
 	LoginPage lpagloginpageObj;
+
 	
 	
 	/*------------------------------------------------BeforClass initialization----------------------------------------------*/
-	@BeforeClass
+	@BeforeMethod
 	public void initilization() {
 		launchWebSite();
 		lpagloginpageObj = new LoginPage(driver);
 	}
+		
 	/*------------------------------------------------Closing the browser after the test ----------------------------------------------*/
-	@AfterClass
+	@AfterMethod
 	public void close() {
 		tearDown();
 	}
@@ -33,5 +38,6 @@ public class LoginPageTest extends TestBase {
 	public void validateforgotpasswordlink() {
 		lpagloginpageObj.clickOnForgotPassword();
 	}
+
 	
 }
