d6fe21146e3641ec1dc0a389890a22509502a5e5
Bikash <info.bikash9@gmail.com>
2024-02-12 17:55:57 +0530

    Added new elements,clickOnCancelButton() and modified clickSaveButton()

--- src/test/java/com/taspro/pages/OnBoardingPage.java
+++ src/test/java/com/taspro/pages/OnBoardingPage.java
@@ -2,6 +2,7 @@ package com.taspro.pages;
 
 import java.util.List;
 
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -57,6 +58,12 @@ public class OnBoardingPage extends PageBase {
 
 	@FindBy(xpath = "//*[@id=\"mat-dialog-0\"]/app-add-candidate/div/div/div[2]/form/div[11]/div/button")
 	private WebElement saveButton;
+
+	@FindBy(xpath = "//a[contains(@class,'btn btn-danger border-0')]")
+	private WebElement cancelButton;
+
+	@FindBy(xpath = "//span[contains(@class,'mat-simple-snack-bar-content')]")
+	private WebElement messagePopUp;
 
 	/*------------------------------------Page initialization----------------------------------------------*/
 	public OnBoardingPage(WebDriver driver) {
@@ -114,8 +121,22 @@ public class OnBoardingPage extends PageBase {
 
 	}
 
-	public void clickSaveButton() {
-		scrollAndClick(saveButton);
+	public String clickSaveButton() {
+
+		try {
+			scrollAndClick(saveButton);
+			waitForElementToBeVisible(messagePopUp);
+			return messagePopUp.getText();
+		} catch (TimeoutException e) {
+			System.out.println("The 'Save' button is not clickable!");
+			clickOnCancelButton();
+			return null;
+		}
+
+	}
+
+	public void clickOnCancelButton() {
+		scrollAndClick(cancelButton);
 	}
 
 }
