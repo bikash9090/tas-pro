e05d25f6db3d16a7591a2e135a6083bc3391214d
Bikash <info.bikash9@gmail.com>
2024-03-05 12:23:58 +0530

    Removed the dulicate code.

--- src/test/java/com/taspro/pages/DashboardPage.java
+++ src/test/java/com/taspro/pages/DashboardPage.java
@@ -13,94 +13,6 @@ public class DashboardPage extends PageBase {
 	@FindBy(xpath = "//*[@id=\"header\"]/div/div[3]/nav/ul/li[1]/button")
 	private WebElement attendanceBtn;
 
-	@FindBy(xpath = "/html/body/app-root/app-dashboard/app-navbar/header/div/div[3]/nav/ul/li[2]/a/i")
-	private WebElement notificationIcon;
-
-	@FindBy(xpath = "//i[@class='bi bi-person']")
-	private WebElement profileIcon;
-
-	@FindBy(xpath = "//span[@class=\"uxg-caption\"]")
-	private WebElement profileDetails;
-
-
-<<<<<<< HEAD
-	@FindBy(xpath = "/html/body/app-root/app-dashboard/app-navbar/div/ul/li[2]/a")
-	private WebElement onBoardingTab;
-
-	/*-------------------------------------Page initialization----------------------------------------------*/
-	public DashboardPage(WebDriver driver) {
-		super(driver);
-		PageFactory.initElements(driver, this);
-	}
-
-	/*----------------------------------------Custom actions---------------------------------------------------*/
-	public boolean isLoginSuccessful(String url) {
-
-		waitForElementToBeVisible(profileIcon);
-		if (driver.getCurrentUrl().equals(url)) {
-			return true;
-		} else {
-			return false;
-		}
-
-	}
-
-	public void clickOnoNtificationbutton() {
-		scrollAndClick(notificationIcon);
-	}
-
-	public void clickOnProfileIconIcon() {
-		waitForElementToBeVisible(profileIcon);
-		scrollAndClick(profileIcon);
-	}
-
-	public void clickOnOnboardingTab() {
-		waitForElementToBeVisible(onBoardingTab);
-		scrollAndClick(onBoardingTab);
-	}
-
-	public String getUsername() {
-		return profileDetails.getText();
-	}
-
-	public void clickCheckInBtn() {
-		String currentBtnText = getCssTextOfElement(attendanceBtn);
-
-		if (currentBtnText.toLowerCase().equalsIgnoreCase("\"Check In\"")) {
-			flashAndClick(attendanceBtn);
-		} else if (currentBtnText.toLowerCase().equalsIgnoreCase("\"Check Out\"")) {
-			System.out.println("Employee has already checked in");
-		} else {
-			System.out.println("Text not matched!");
-		}
-	}
-
-	public void clickCheckOutBtn() {
-		String currentBtnText = getCssTextOfElement(attendanceBtn);
-
-		if (currentBtnText.toLowerCase().equalsIgnoreCase("\"Check Out\"")) {
-			flashAndClick(attendanceBtn);
-		} else if (currentBtnText.toLowerCase().equalsIgnoreCase("\"Check In\"")) {
-			System.out.println("Employee has not Checked In yet!");
-		} else {
-			System.out.println("Text not matched!");
-		}
-	}
-}
-=======
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-
-import com.taspro.base.PageBase;
-
-public class DashboardPage extends PageBase {
-
-	/*----------------------------------------Page objects----------------------------------------------*/
-	@FindBy(xpath = "//*[@id=\"header\"]/div/div[3]/nav/ul/li[1]/button")
-	private WebElement attendanceBtn;
-
 	@FindBy(css = ".scrollto.btn.btn-checkin.checkin.ng-star-inserted")
 	private WebElement checkInbutton;
 
@@ -179,4 +91,3 @@ public class DashboardPage extends PageBase {
 		}
 	}
 }
->>>>>>> master
