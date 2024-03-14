c6a05d649afa79f50e2c99372be71c4dc6053a6b
Bikash <info.bikash9@gmail.com>
2024-03-14 15:54:28 +0530

    Renamed runner

---
+++ src/test/java/com/taspro/runners/Runner.java
@@ -0,0 +1,8 @@
+package com.taspro.runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = "src/test/resources/features/forgotpasswordpage.feature", glue = { "com.taspro.stepdefinations" })
+public class Runner extends AbstractTestNGCucumberTests {
+	}
